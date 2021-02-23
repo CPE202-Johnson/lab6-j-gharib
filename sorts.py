6e07a60575bdddec795669383df87de68350ba1e
j-gharib <jgharib@calpoly.edu>
2021-02-23 11:57:59 -0800

    Check if calculating correct number of comparisons

--- sorts.py
+++ sorts.py
@@ -22,19 +22,16 @@ def selection_sort(listy):
 def insertion_sort(listy):
     comparisons = 0
     for i in range(len(listy) - 1):
-        comparisons += 1
         sortInd = i + 1
-        if listy[sortInd] < listy[i]:
-            j = i
-            ind2 = sortInd
-            while j >= 0:
-                comparisons += 1
-                if listy[j] > listy[ind2]:
-                    temp = listy[ind2]
-                    listy[ind2] = listy[j]
-                    listy[j] = temp
-                ind2 -= 1
-                j -= 1
+        sortVal = listy[sortInd]
+        j = i
+        while(sortVal < listy[j]) and (j >= 0):
+            comparisons += 1
+            listy[sortInd] = listy[j]
+            listy[j] = sortVal
+            j -= 1
+            sortInd -= 1
+            sortVal = listy[sortInd]
     return comparisons
 
 
@@ -45,14 +42,19 @@ def main():
     # Give the random number generator a seed, so the same sequence of 
     # random numbers is generated at each run
     
-    #random.seed(1234) 
-    
+    random.seed(1234) 
+    insertion_sort([6, 5, 4, 3, 2, 1])
     # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 1000)
+    totalComps = 0
     start_time = time.time() 
-    comps = insertion_sort(randoms)
+    for i in range(10):
+        randoms = random.sample(range(1000000), 2000)
+        totalComps += insertion_sort(randoms)
+        #totalComps += selection_sort(randoms)
     stop_time = time.time()
-    print(comps, stop_time - start_time)
+    totalComps /= 10
+    print("Avg comps: " + str(totalComps) + " | Avg time: " + str("{0:0.3f}".format((stop_time - start_time)/10 * 1000)) + "ms")
 
+    
 if __name__ == '__main__': 
     main()
