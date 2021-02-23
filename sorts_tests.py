40ad85656974d9a7563c2b7f8d2b8befc02cdf35
j-gharib <jgharib@calpoly.edu>
2021-02-23 11:59:03 -0800

    Check if calculating correct number of comparisons

--- sorts_tests.py
+++ sorts_tests.py
@@ -8,13 +8,19 @@ class TestLab4(unittest.TestCase):
         comps = selection_sort(nums)
         self.assertEqual(comps, 1)
         self.assertEqual(nums, [10, 23])
+        nums = [23, 10]
+        comps = insertion_sort(nums)
+        self.assertEqual(comps, 1)
+        self.assertEqual(nums, [10, 23])
     
     def test_moderate(self):
         nums_selection = [6, 5, 4, 3, 2, 1]
         nums_insertion = [6, 5, 4, 3, 2, 1]
         comps1 = selection_sort(nums_selection)
         comps2 = insertion_sort(nums_insertion)
+        self.assertEqual(comps1, 15)
         self.assertEqual(nums_selection, [1, 2, 3, 4, 5, 6])
+        self.assertEqual(comps2, 15)
         self.assertEqual(nums_insertion, [1, 2, 3, 4, 5, 6])
 
 
