ec8ed22c92bd79677d53a61572178cd981f16d7c
j-gharib <jgharib@calpoly.edu>
2021-02-23 11:08:10 -0800

    Check if calculating correct number of comparisons

--- sorts.py
+++ sorts.py
@@ -1,21 +1,55 @@
 import random
 import time
 
-def selection_sort(list):
-    pass
+def selection_sort(listy):
+    comparisons = 0
+    if len(listy) == 0:
+        return listy
+    ind = -1
+    for i in range(len(listy)):
+        j = i + 1
+        minVal = listy[i]
+        while j < len(listy):
+            if listy[j] < minVal:
+                ind = j
+                minVal = listy[j]
+                comparisons += 1
+            j += 1
+        if minVal < listy[i]:
+            listy[ind] = listy[i]
+            listy[i] = minVal
+    return comparisons
+
     
-def insertion_sort(list):
-    pass
-   
+def insertion_sort(listy):
+    comparisons = 0
+    for i in range(len(listy) - 1):
+        sortInd = i + 1
+        if listy[sortInd] < listy[i]:
+            j = i
+            ind2 = sortInd
+            while j >= 0:
+                if listy[j] > listy[ind2]:
+                    comparisons += 1
+                    temp = listy[ind2]
+                    listy[ind2] = listy[j]
+                    listy[j] = temp
+                ind2 -= 1
+                j -= 1
+    return comparisons
+
+
+
 
 def main():
     # Code coverage NOT required for main
     # Give the random number generator a seed, so the same sequence of 
     # random numbers is generated at each run
-    random.seed(1234) 
+    
+    #random.seed(1234) 
     
     # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 5000)
+    randoms = random.sample(range(1000000), 1000)
     start_time = time.time() 
     comps = selection_sort(randoms)
     stop_time = time.time()
@@ -23,4 +57,3 @@ def main():
 
 if __name__ == '__main__': 
     main()
-
